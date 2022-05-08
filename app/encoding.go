39e4206f5aff4f7de37265dfac685ec37960bfcd
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-05-08 14:48:50 +0900

    Create EncodingConfig

---
+++ app/encoding.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+)
+
+/*
+EncodingConfig 与えられたアプリで使用する具体的なエンコーディングタイプを指定します。
+これは、protobufとaminoの実装間の互換性のために提供されます。
+InterfaceRegistry
+Protobufコーデックがgoogle.protobuf.Anyを使ってエンコードおよびデコードされたインターフェースを扱うために使用
+Anyから安全に展開できるインターフェースと実装を登録するための機構を提供
+Codec
+SDK全体で使用されるデフォルトのコーデック
+状態のエンコードとデコードに使用
+TxConfig
+クライアントがアプリケーション定義の具体的なトランザクションタイプを生成するために利用できるインターフェイスを定義
+LegacyAmino
+古いコーデック(削除予定)
+*/
+type EncodingConfig struct {
+	InterfaceRegistry types.InterfaceRegistry
+	Codec             codec.Codec
+	TxConfig          client.TxConfig
+	LegacyAmino       *codec.LegacyAmino
+}
